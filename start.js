24f428980908157317e9e187691796e78de4e06e
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-05-12 13:22:05 -0400

    Initial commit

---
+++ start.js
@@ -0,0 +1,9 @@
+const shuffleBtn = document.getElementById('shuffleBtn');
+const items = [...document.querySelectorAll('.grid-item')];
+
+shuffleBtn.addEventListener('click', () => {
+  items.forEach((item) => {
+    const random = Math.floor(Math.random() * 4);
+    item.setAttribute('style', `order: ${random}`);
+  });
+});
